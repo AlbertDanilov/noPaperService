92097c1d466d4cd1a8e5b8967379e3bd4be6f5a6
Albert Danilov <albert95danilov@gmail.com>
2022-04-20 08:20:50 +0300

    test project

---
+++ noPaperService_test/Program.cs
@@ -0,0 +1,25 @@
+﻿using noPaperService_common.Entities;
+using noPaperService_ecpWorker;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noPaperService_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //читаем подпись в массив байтов
+            byte[] sign = File.ReadAllBytes("C:\\Users\\user\\Desktop\\RMQ\\1585758.p7s");
+
+            //считываем подписи из файла
+            List<SignComponent> signersList = X509.PKCS_7.GetSigners(sign);
+
+            Console.ReadLine();
+        }
+    }
+}
