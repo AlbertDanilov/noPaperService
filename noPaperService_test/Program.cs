c96a2065ee214f77b53de6af5f0e7d95f43a52c1
Albert Danilov <albert95danilov@gmail.com>
2022-04-20 08:31:06 +0300

    sign valid

--- noPaperService_test/Program.cs
+++ noPaperService_test/Program.cs
@@ -13,11 +13,20 @@ namespace noPaperService_test
     {
         static void Main(string[] args)
         {
+            //читаем подписанный файл
+            byte[] signedFile = File.ReadAllBytes("C:\\Users\\user\\Desktop\\RMQ\\1585758.json");
+
             //читаем подпись в массив байтов
             byte[] sign = File.ReadAllBytes("C:\\Users\\user\\Desktop\\RMQ\\1585758.p7s");
 
             //считываем подписи из файла
             List<SignComponent> signersList = X509.PKCS_7.GetSigners(sign);
+
+            //проверяем валидность подписи
+            bool valid = X509.PKCS_7.Detached.Verify(signedFile, sign);
+
+            //полная првоерка валидности подписи
+            int validFull = X509.PKCS_7.Detached.fullVreify(signedFile, sign);
 
             Console.ReadLine();
         }
