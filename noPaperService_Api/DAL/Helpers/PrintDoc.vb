536b02ee49697c0b45b70d0280b38d7adb12510a
Алмаз Шагиев <almazz1997@gmail.com>
2022-04-25 10:36:47 +0300

    печать

--- noPaperService_Api/DAL/Helpers/PrintDoc.vb
+++ noPaperService_Api/DAL/Helpers/PrintDoc.vb
@@ -9,8 +9,6 @@ Public Class PrintDoc
         Dim json As String = File.ReadAllText(jsonFileNamePath)
 
         Dim document1 As EcpSignData_pv = JsonConvert.DeserializeObject(Of EcpSignData_pv)(json)
-        Dim document2 As EcpSignData_pvs = JsonConvert.DeserializeObject(Of EcpSignData_pvs)(json)
-        Dim document3 As EcpSignData_ttns = JsonConvert.DeserializeObject(Of EcpSignData_ttns)(json)
 
         docFileName = $"Накладная {document1.pv_nom} от ({Date.Now.ToString("dd.MM.yyyy HH.mm.ss")})"
         docFileNamePath = $"{mainPath}\{docFileName}"
@@ -22,12 +20,27 @@ Public Class PrintDoc
 
         Dim nameBookmark As Bookmark = document.Bookmarks("document_num")
         document.Replace(nameBookmark.Range, document1.pv_num)
-        nameBookmark = document.Bookmarks("agent_recipient")
+        nameBookmark = document.Bookmarks("pv_agent_agnabbr")
         document.Replace(nameBookmark.Range, document1.pv_agent_agnabbr)
+        nameBookmark = document.Bookmarks("pv_plat_agnabbr")
+        document.Replace(nameBookmark.Range, document1.pv_plat_agnabbr)
         nameBookmark = document.Bookmarks("create_date")
         document.Replace(nameBookmark.Range, document1.pv_create_date.Value.ToString())
         nameBookmark = document.Bookmarks("otgr_date")
         document.Replace(nameBookmark.Range, document1.pv_otg_date.Value.ToString())
+        nameBookmark = document.Bookmarks("ttns_shifr")
+
+        For Each i As EcpSignData_pvs In document1.pvsList
+            document.Replace(nameBookmark.Range, i.ttnsInfo.ttns_shifr)
+            nameBookmark = document.Bookmarks("ttns_nommodif")
+            document.Replace(nameBookmark.Range, If(i.ttnsInfo.ttns_p_name_s, i.ttnsInfo.ttns_nommodif))
+            nameBookmark = document.Bookmarks("ttns_prcena_bnds")
+            document.Replace(nameBookmark.Range, i.ttnsInfo.ttns_prcena_bnds)
+            nameBookmark = document.Bookmarks("ttns_ocena_nds")
+            document.Replace(nameBookmark.Range, i.ttnsInfo.ttns_ocena_nds)
+            nameBookmark = document.Bookmarks("ttns_rcena_nds")
+            document.Replace(nameBookmark.Range, i.ttnsInfo.ttns_rcena_nds)
+        Next
 
         server.SaveDocument(docFileNamePathExtension, DocumentFormat.OpenXml)
 
